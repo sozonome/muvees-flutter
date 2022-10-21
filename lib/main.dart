04a744c8fe5e1a5d07bd47063d38b1b7acc08902
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-10-21 12:06:47 +0700

    chore: setup google_fonts - outfit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:muvees/config/routes/router.dart';
 
 void main() {
+  GoogleFonts.config.allowRuntimeFetching = false;
   runApp(const MyApp());
 }
 
@@ -11,6 +13,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
     return MaterialApp.router(
       title: 'muvees',
       theme: ThemeData(
@@ -23,7 +27,8 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
+        textTheme: GoogleFonts.outfitTextTheme(textTheme),
       ),
       routerConfig: AppRouter.router,
     );
