c36867ef604f29d8b1c28de216f030e1168e38ec
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-11-05 21:52:37 +0700

    refactor: set includeifnull false

--- lib/core/models/api/tmdb/movie/movie_list.dart
+++ lib/core/models/api/tmdb/movie/movie_list.dart
@@ -3,7 +3,7 @@ import 'package:muvees/core/models/api/tmdb/tmdb_list_response.dart';
 
 part 'movie_list.g.dart';
 
-@JsonSerializable()
+@JsonSerializable(includeIfNull: false)
 class MovieListParams {
   MovieListParams({
     this.language,
@@ -89,14 +89,14 @@ class MovieListItemType {
 
   @JsonKey(name: 'backdrop_path')
   final String? backdropPath;
-  final int popularity;
+  final double popularity;
 
   @JsonKey(name: 'vote_count')
   final int voteCount;
   final bool video;
 
   @JsonKey(name: 'vote_average')
-  final int voteAverage;
+  final double voteAverage;
 
   Map<String, dynamic> toJson() => _$MovieListItemTypeToJson(this);
 }
