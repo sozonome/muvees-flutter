d7fba53b30efaec1fd1f3c0185b34543cf7923d0
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-01-13 18:42:54 +0700

    chore: setup alice notification

--- lib/core/config/services/alice.dart
+++ lib/core/config/services/alice.dart
@@ -5,4 +5,5 @@ import 'package:muvees/core/constants/navigator_key.dart';
 Alice aliceConfig = Alice(
   navigatorKey: navigatorKey,
   showInspectorOnShake: kDebugMode,
+  showNotification: kDebugMode,
 );
