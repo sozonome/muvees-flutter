1018d5aa3414cfc205c0bf39b3ace7bd5ac29551
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-05-16 12:25:30 +0700

    chore: update to flutter 3.10 and dart 3.0

--- lib/core/config/services/alice.dart
+++ lib/core/config/services/alice.dart
@@ -1,9 +1,9 @@
-import 'package:alice/alice.dart';
-import 'package:flutter/foundation.dart';
+import 'package:alice_lightweight/alice.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:muvees/core/constants/navigator_key.dart';
 
 Alice aliceConfig = Alice(
   navigatorKey: navigatorKey,
-  showInspectorOnShake: kDebugMode,
-  showNotification: kDebugMode,
+  // showInspectorOnShake: kDebugMode,
+  // showNotification: kDebugMode,
 );
